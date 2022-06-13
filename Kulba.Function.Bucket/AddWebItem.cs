17a6acd3cf6d1d7c04d9d61a9a4786071e7c514a
Jim Kulba <jkulba@gmail.com>
2022-06-12 22:36:42 -0700

    Update to Trigger payload

--- Kulba.Function.Bucket/AddWebItem.cs
+++ Kulba.Function.Bucket/AddWebItem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -14,22 +15,48 @@ namespace Kulba.Function.Bucket
     {
         [FunctionName("AddWebItem")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Create new Web Item request.");
 
-            string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            WebItemMessage message = new()
+            {
+                Id = Guid.NewGuid(),
+                Title = data?.Name,
+                CommandType = data?.CommandType,
+                Url = data?.Url,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            return (ActionResult)new OkObjectResult(new
+            {
+                returnMessage = "Success Add New Web Item",
+                returnStatusCode = 0,
+                MessageId = message.Id,
+                MessageTitle = message.Title,
+                MessageType = message.CommandType,
+                MessageUrl = message.Url,
+                CreatedDate = message.CreatedDate
+            });
         }
+    }
+
+    public record WebItemMessage
+    {
+        public Guid Id { get; init; }
+        [Required]
+        public string CommandType { get; init; }
+        [Required]
+        public string Title { get; init; }
+        [Required]
+        public string Url { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
+
     }
 }
